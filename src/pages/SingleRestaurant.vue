5e7454c4beefe2d3a8afbdaf71c120033f826d98
danielecarpentiero <carpentierodaniele@gmail.com>
2024-03-06 16:09:43 +0100

    feat: single detail

--- src/pages/SingleRestaurant.vue
+++ src/pages/SingleRestaurant.vue
@@ -1,10 +1,58 @@
 <script>
+import { RouterView } from "vue-router";
+import AppHeader from "../components/AppHeader.vue";
+import AppFooter from "../components/AppFooter.vue";
+import axios from "axios";
+import store from "../store";
+
 export default {
   name: "SingleRestaurant",
-  components: {},
+  props: ["restaurant"],
+  components: {
+    AppHeader,
+    RouterView,
+    AppFooter,
+  },
+  data() {
+    return {
+      restaurants: [],
+      store,
+    };
+  },
+  methods: {
+    getRestaurants() {
+      console.log(this.store);
+      axios
+        .get(
+          this.store.api.mainUrl +
+            this.store.api.listUrl.restaurants +
+            "/" +
+            this.$route.params.slug
+        )
+        .then((response) => {
+          this.restaurants = response.data.results.data;
+          console.log(this.restaurants);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+  created() {
+    this.getRestaurants();
+    console.log(this.getRestaurants);
+    console.log(this.$route);
+  },
 };
 </script>
 
-<template></template>
+<template>
+  <div class="card">
+    <div class="card-body">
+      <h2 class="card-title">{{ restaurants.name }}</h2>
+      <h3>{{ restaurants.address }}</h3>
+    </div>
+  </div>
+</template>
 
 <style></style>
