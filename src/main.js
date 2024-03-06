d10fbb165021268385200d0cd1b8e300a1bd43b9
lorenzo91347 <lorenzo.losito91@gmail.com>
2024-03-06 10:50:39 +0100

    feat:scaffolding

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import { createApp } from 'vue'
-import './style.css'
+
+import './style.scss'
+
+import * as bootstrap from 'bootstrap';
+
+import { router } from './router';
+
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
