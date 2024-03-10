f36bffa6799aac0262b6a33876dad7cc855c0bd5
DaviGel <geloso.davide@gmail.com>
2024-03-10 10:33:20 +0100

    feaet: rimosso watch

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -1,8 +1,10 @@
 <script>
 import store from '../store';
+import { myMixin } from '../myMixin';
 
 export default {
     name: 'Cart',
+    mixins: [myMixin],
 
     data() {
         return {
@@ -25,22 +27,6 @@ export default {
             this.store.cart.items[index].quantity++;
         },
     },
-
-    watch: {
-        'store.cart.items': {
-            handler() {
-                let subtotal = 0; // Inizializza il subtotal come variabile locale
-                this.store.cart.items.forEach(element => {
-                    const price = parseFloat(element.price);
-                    if (!isNaN(price)) {
-                        subtotal += price * element.quantity; // Aggiungi il prezzo senza arrotondare
-                    }
-                });
-                this.store.cart.subtotal = subtotal.toFixed(2); // Assegna il subtotal arrotondato a due decimali
-            },
-            deep: true // Assicurati di osservare le modifiche in profondità
-        }
-    }
 };
 
 </script>
@@ -79,4 +65,4 @@ export default {
     </ul>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss"></style>../myMixin
