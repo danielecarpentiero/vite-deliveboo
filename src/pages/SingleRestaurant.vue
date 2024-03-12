a64639047c125103cc84a77cf4dada0713b60807
DaviGel <geloso.davide@gmail.com>
2024-03-12 11:39:15 +0100

    fix: sistemato if

--- src/pages/SingleRestaurant.vue
+++ src/pages/SingleRestaurant.vue
@@ -71,8 +71,8 @@ export default {
       axios
         .get(
           this.store.api.mainUrl +
-            this.store.api.listUrl.restaurants +
-            this.$route.params.slug
+          this.store.api.listUrl.restaurants +
+          this.$route.params.slug
         )
         .then((response) => {
           this.restaurant = response.data.result;
@@ -167,25 +167,16 @@ export default {
             <h3 class="mt-2 text-center">Menu</h3>
 
             <ul class="list-unstyled row gap-3 justify-content-center">
-              <li
-                v-for="food in restaurant.foods"
-                :key="food.id"
-                class="col-12 col-md-3 text-center flip-card mt-3 rounded-5"
-              >
+              <li v-for="food in restaurant.foods" :key="food.id"
+                class="col-12 col-md-3 text-center flip-card mt-3 rounded-5">
                 <div class="flip-card-inner shadow rounded-5">
                   <div class="w-100 flip-card-front rounded-5">
-                    <img
-                      :src="
-                        store.api.mainUrl + store.api.storagePath + food.img
-                      "
-                      :alt="food.slug"
-                      class="rounded-5"
-                    />
+                    <img :src="store.api.mainUrl + store.api.storagePath + food.img
+                " :alt="food.slug" class="rounded-5" />
                   </div>
                   <!-- Flip card -->
                   <div
-                    class="flip-card-back rounded-5 d-flex flex-column justify-content-center align-items-center gap-2"
-                  >
+                    class="flip-card-back rounded-5 d-flex flex-column justify-content-center align-items-center gap-2">
                     <div class="info">
                       <h3 class="mt-5 mb-2 text-uppercase">{{ food.name }}</h3>
                       <p>Description: {{ food.description }}</p>
@@ -194,10 +185,7 @@ export default {
                       <p v-else>Non vegetariano</p>
                       <p v-if="food.is_visible">Elemento disponibile</p>
                       <p v-else>Elemento non disponibile</p>
-                      <button
-                        class="btn btn-success w-30 mt-4"
-                        @click="addItemToCart(food, restaurant)"
-                      >
+                      <button class="btn btn-success w-30 mt-4" @click="addItemToCart(food, restaurant)">
                         <font-awesome-icon :icon="['fas', 'plus']" />
                       </button>
                     </div>
@@ -210,37 +198,21 @@ export default {
               <div class="modal-dialog">
                 <div class="modal-content">
                   <div class="modal-header">
-                    <h1
-                      class="modal-title fs-5 text-dark"
-                      id="exampleModalLabel"
-                    >
+                    <h1 class="modal-title fs-5 text-dark" id="exampleModalLabel">
                       Emptying cart
                     </h1>
-                    <button
-                      type="button"
-                      class="btn-close"
-                      data-bs-dismiss="modal"
-                      aria-label="Close"
-                    ></button>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                   </div>
                   <div class="modal-body text-dark text-start">
                     You can order from one restaurant at a time. Do you want to
                     empty your cart and insert "{{ this.selectedFoodName }}"?
                   </div>
                   <div class="modal-footer">
-                    <button
-                      type="button"
-                      class="btn btn-secondary text-white"
-                      data-bs-dismiss="modal"
-                    >
+                    <button type="button" class="btn btn-secondary text-white" data-bs-dismiss="modal">
                       Close
                     </button>
-                    <button
-                      type="button"
-                      class="btn btn-danger text-white"
-                      @click="emptyAndReplaceCart()"
-                      data-bs-dismiss="modal"
-                    >
+                    <button type="button" class="btn btn-danger text-white" @click="emptyAndReplaceCart()"
+                      data-bs-dismiss="modal">
                       Empty cart
                     </button>
                   </div>
@@ -310,11 +282,9 @@ img {
 /* Style the back side */
 .flip-card-back {
   background: rgb(255, 134, 20);
-  background: linear-gradient(
-    118deg,
-    rgba(255, 134, 20, 1) 0%,
-    rgba(64, 74, 172, 1) 98%
-  );
+  background: linear-gradient(118deg,
+      rgba(255, 134, 20, 1) 0%,
+      rgba(64, 74, 172, 1) 98%);
   color: white;
   transform: rotateY(180deg);
 
