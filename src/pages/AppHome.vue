3649689ac4dc9d35bbfa249381bb48bfabf535cf
mihaipaulorban <mihaipaulorban@gmail.com>
2024-03-15 15:31:15 +0100

    feat: work section

--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -228,9 +228,9 @@ export default {
       ></path>
     </svg>
   </div>
-  <div class="container">
+  <div class="container mb-5">
     <!-- tipi -->
-
+    <h2 class="display-4 text-center mb-5">What do you want to eat today?</h2>
     <div class="container-filters mt-3">
       <!-- Filtri per mobile -->
       <ul v-if="isMobile" class="list-unstyled">
@@ -333,6 +333,69 @@ export default {
         </RouterLink>
       </li>
     </ul>
+  </div>
+  <div class="custom-shape-divider-bottom-1710512821">
+    <svg
+      data-name="Layer 1"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 1200 120"
+      preserveAspectRatio="none"
+    >
+      <path
+        d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z"
+        class="shape-fill"
+      ></path>
+    </svg>
+  </div>
+  <div class="row p-5 work-section">
+    <div class="col-12 col-md-6 pr">
+      <div class="container-img text-center">
+        <img
+          class="img-fluid my-img"
+          style="width: 500px"
+          src="https://img.freepik.com/free-photo/delivery-concept-handsome-african-american-delivery-man-rush-running-delivering-package-customer-isolated-grey-studio-background-copy-space_1258-1211.jpg"
+          alt=""
+        />
+      </div>
+    </div>
+    <div class="col-12 text-center col-md-6 d-flex align-items-center">
+      <div>
+        <h1 class="fw-bold">Work with us.</h1>
+
+        <div class="container p-3 mt-3">
+          <h3>
+            We are passionate about working with the best restaurants and chefs
+            to deliver high quality, delicious meals directly to your door. Our
+            delivery partners care about providing an exceptional service to get
+            your food to you promptly and safely. Together, we strive to make
+            your food delivery experience easy and enjoyable.
+          </h3>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="custom-shape-divider-top-1710512768 mb-5">
+    <svg
+      data-name="Layer 1"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 1200 120"
+      preserveAspectRatio="none"
+    >
+      <path
+        d="M0,0V46.29c47.79,22.2,103.59,32.17,158,28,70.36-5.37,136.33-33.31,206.8-37.5C438.64,32.43,512.34,53.67,583,72.05c69.27,18,138.3,24.88,209.4,13.08,36.15-6,69.85-17.84,104.45-29.34C989.49,25,1113-14.29,1200,52.47V0Z"
+        opacity=".25"
+        class="shape-fill"
+      ></path>
+      <path
+        d="M0,0V15.81C13,36.92,27.64,56.86,47.69,72.05,99.41,111.27,165,111,224.58,91.58c31.15-10.15,60.09-26.07,89.67-39.8,40.92-19,84.73-46,130.83-49.67,36.26-2.85,70.9,9.42,98.6,31.56,31.77,25.39,62.32,62,103.63,73,40.44,10.79,81.35-6.69,119.13-24.28s75.16-39,116.92-43.05c59.73-5.85,113.28,22.88,168.9,38.84,30.2,8.66,59,6.17,87.09-7.5,22.43-10.89,48-26.93,60.65-49.24V0Z"
+        opacity=".5"
+        class="shape-fill"
+      ></path>
+      <path
+        d="M0,0V5.63C149.93,59,314.09,71.32,475.83,42.57c43-7.64,84.23-20.12,127.61-26.46,59-8.63,112.48,12.24,165.56,35.4C827.93,77.22,886,95.24,951.2,90c86.53-7,172.46-45.71,248.8-84.81V0Z"
+        class="shape-fill"
+      ></path>
+    </svg>
   </div>
 </template>
 
@@ -624,5 +687,41 @@ export default {
 .slick-slide img {
   width: 100%;
   padding: 15px;
+}
+
+.my-img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+  border-radius: 15px;
+}
+.work-section {
+  background-color: $darkgreen;
+  color: $beige;
+}
+.custom-shape-divider-bottom-1710512821 svg {
+  position: relative;
+  display: block;
+  top: 10px;
+  width: calc(157% + 1.3px);
+  height: 157px;
+  transform: rotateY(180deg);
+}
+
+.custom-shape-divider-bottom-1710512821 .shape-fill {
+  fill: #386641;
+}
+
+.custom-shape-divider-top-1710512768 svg {
+  position: relative;
+  display: block;
+  width: calc(157% + 1.3px);
+  height: 157px;
+  transform: rotateY(180deg);
+}
+
+.custom-shape-divider-top-1710512768 .shape-fill {
+  fill: #386641;
 }
 </style>
