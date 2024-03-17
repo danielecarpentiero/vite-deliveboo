ddea465207b8946b5aced5804105f4eb88922aab
danielecarpentiero <carpentierodaniele@gmail.com>
2024-03-17 18:15:01 +0100

    fix: successful h1

--- src/pages/Successful.vue
+++ src/pages/Successful.vue
@@ -24,7 +24,7 @@ export default {
       class="d-flex flex-wrap p-2 justify-content-center flex-column mt-5 mb-5 gap-3 container"
     >
       <h1 class="display-1 text-white text-center mb-4">
-        Your order was successful
+        Your order has been sent
       </h1>
       <font-awesome-icon
         :icon="['far', 'circle-check']"
