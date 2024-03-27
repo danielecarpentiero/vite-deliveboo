642c0390a11c995e2c210248ba0522355fd0b253
DaviGel <geloso.davide@gmail.com>
2024-03-27 12:19:46 +0100

    feat: filter button close

--- src/pages/SingleRestaurant.vue
+++ src/pages/SingleRestaurant.vue
@@ -519,6 +519,8 @@ img {
 }
 
 .btn-close {
+  filter: brightness(0) saturate(100%) invert(100%) sepia(100%) saturate(0%) hue-rotate(288deg) brightness(102%) contrast(102%);
+
   &:hover {
     color: #fff;
   }
